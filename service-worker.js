8ee8dead85eff8c60ecc6fe0cde58f52b7acec3c
InsertCleverNameHere <mol7d2bnws5a@gmail.com>
2025-10-20 17:10:00 +0300

    caching actually works

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,46 @@
-const CACHE_NAME = 'groove-trainer-v1';
+const CACHE_NAME = "groove-trainer-v1";
 const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  '.css/styles.css',
-  '.js/main.js',
-  './commits.json',
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/styles.css",
+  "./js/main.js",
+  "./commits.json",
 ];
 
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(FILES_TO_CACHE)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((keys) =>
+        Promise.all(
+          keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+        )
+      )
+  );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches
+        .match("./index.html")
+        .then((cachedResponse) => {
+          return cachedResponse || fetch(event.request);
+        })
+        .catch(() => caches.match("./index.html"))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  }
 });
